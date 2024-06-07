9b9368a24fc231914a78e92f4148f9dd3a6d9af9
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-07 16:12:22 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+//we are using node.js
+console.log("binit")
+let Name="binit"
+let age =16
+let isLoggedIn=false
+//boolean=>true/false
+//string=>" "
+//null=>stand alone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof age);
+console.log(typeof Name);
