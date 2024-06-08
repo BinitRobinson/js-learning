6da90e09554e1d1aedf72fc5e30cbad92044db81
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-08 05:56:32 +0000

    conversion of datatype

---
+++ 01_basics/typeconversion.js
@@ -0,0 +1,30 @@
+//let score=true;
+//console.log(typeof score);
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+
+///let isLoggedin=1;
+//let valueLogged=Boolean(isLoggedin);
+//console.log( typeof valueLogged);
+//console.log(valueLogged);
+
+//1=>true
+//0=>false
+//""=>false
+//"binit"=>true
+
+let number=33;
+let stringnumber=String(number);
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
