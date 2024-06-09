07efe5fea99afadb6d207ffb68d3c9d61005bdb9
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-09 01:16:41 +0000

    datatype summary

---
+++ 01_basics/datatype2.js
@@ -0,0 +1,12 @@
+//primitive data type
+// 7 types:string,number ,boolean,null ,undefined,symbol,BigInt
+
+//null=>completely empty
+
+const id=Symbol('123')
+const logId=Symbol('123')
+
+console.log( id==logId);
+
+//reference type(non primitive)
+//arrays,objects ,functions
