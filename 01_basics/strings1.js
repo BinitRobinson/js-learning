91d895448be40e284184b8e6aef82bdb67d31fc5
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-09 03:16:26 +0000

    string operation

---
+++ 01_basics/strings1.js
@@ -0,0 +1,21 @@
+const name ="binit";
+const age =22;
+
+//console.log(`hello my name is ${name} and my age is ${age}`);
+//console.log(name[0]);//accessing the specific value of the string 
+//console.log(name.length);//calculating the length of the string =5
+//console.log(name.toUpperCase());//converts the whole string into upper case
+//console.log(name.charAt(3))//checking if the specific character exist in the string or not in what position =i
+console.log(name.indexOf('t'));//checking the index of the character=4
+
+//const namechange=name.slice(-5,2);
+//console.log (namechange);
+
+const car="  maruti  ";
+//console.log(car.trim());
+//console.log(car);
+
+const url= "hhtp://binit.com/binit%20robinson"
+console.log(url.replace('%20','-'));
+console.log(url.includes('kachhap'));
+console.log(url.includes('binit'));
