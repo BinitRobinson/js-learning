3a029387bfd199a4a16ae048688c834915d213eb
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-09 02:29:28 +0000

    memory in js

---
+++ 01_basics/memory.js
@@ -0,0 +1,9 @@
+//primitive data types mai copy milta hai (stack memory use )
+//non primitive   data type main orignal value hota hai(heap memeory)
+let myname="binit";
+
+let yourname =myname;
+myname="raj";
+
+console.log(yourname);
+console.log(myname);
