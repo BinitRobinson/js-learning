d5fdbc125be8ad9fda5ae3c2d37d4634990a5def
Binit Robinson Kachhap <124648878+BinitRobinson@users.noreply.github.com>
2024-06-19 15:59:09 +0000

    math and num

---
+++ maths.js
@@ -0,0 +1,6 @@
+//console.log(Math.abs(-4));//convert the negative value to positive 
+//console.log(Math.round(3.6));//round off the value 
+//console.log(Math.ceil(3.6));
+//console.log(Math.floor(3.6));
+//console.log(Math.sqrt(3));
+console.log(Math.max(3,4,1,5,));
